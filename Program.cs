00f1214b3e0e4b9b5a05090e77c6e204e429f5c9
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2021-09-05 23:07:39 -0300

    CAI_EjercicioRespaso1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_2021_EjerciciosDeRepaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int nro_registro = 644;
+            string contador = "";
+
+            do
+            {
+                numero = numero + 1;
+                if (numero % 3 == 0)
+                {
+                    if (numero % 5 == 0)
+                    {
+                        contador = contador + numero + " - FooBar" + "\n";
+                    }
+                    else
+                    {
+                        contador = contador + numero + " - Foo" + "\n";
+                    }
+                }
+                else
+                {
+                    contador = contador + numero + "\n";
+                }
+
+                
+            }
+            while (numero < nro_registro);
+
+            Console.WriteLine(contador);
+            Console.ReadKey(); 
+        }
+    }
+}

c553ff1a04afce4ce1c3a65b8bd225eb0270d4a2
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2021-09-07 00:04:58 -0300

    EjercicioRepaso6

---
+++ EjercicioDeRepaso/EjercicioDeRepaso.EjercicioRepaso6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDeRepaso.EjercicioRepaso6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+          
+
+            Console.WriteLine("Por favor, ingrese un numero ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Por favor, ingrese otro numero ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El resultado de la formula es: " + ((a + b)* (a - b)));
+            Console.ReadKey();
+        }
+
+        }
+    }
+

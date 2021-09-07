c8ae7de41cfb169fcd3755c4721559e4ea3b44f0
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2021-09-06 23:48:29 -0300

    EjercicioRepaso5

---
+++ EjercicioDeRepaso/EjerciciosDeRepaso.EjercicioRepaso5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosDeRepaso.EjercicioRepaso5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "";
+           
+            string inverso = "";
+            string caracter;
+            int largo = 0;
+            int i;
+            bool control = false;
+            do 
+            {
+                Console.WriteLine("Ingrese un numero de 5 cifras");
+                num = Console.ReadLine();
+                largo = num.Length;
+                if (largo==5)
+                {
+                    control = true;
+                }
+            }
+            while (control==false);
+            
+         
+                for (int x = largo - 1; x >= 0; x--)
+                {
+                    caracter = num.Substring(x, 1);
+                    inverso = inverso + caracter;
+                }
+                Console.WriteLine("El número invertido es: " + inverso);
+             
+            Console.ReadKey();
+        }
+    }
+}

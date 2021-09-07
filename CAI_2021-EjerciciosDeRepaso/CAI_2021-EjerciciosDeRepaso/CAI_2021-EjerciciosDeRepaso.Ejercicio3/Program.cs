5a5d421e01249fee9204eaf6e352ac3e2f5108bb
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2021-09-06 21:30:29 -0300

    EjercicioRepaso.Ejercicio3

---
+++ CAI_2021-EjerciciosDeRepaso/CAI_2021-EjerciciosDeRepaso/CAI_2021-EjerciciosDeRepaso.Ejercicio3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_2021_EjerciciosDeRepaso.Ejercicio3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string palabra = "";
+            string inverso = "";
+            string caracter;
+            int i;
+           
+                Console.WriteLine("Ingrese una palabra");
+                palabra = Convert.ToString(Console.ReadLine());
+                i = palabra.Length;
+                 for (int x=i-1; x >= 0; x--) 
+                {
+                    caracter = palabra.Substring(x, 1);
+                    inverso = inverso + caracter;
+                }
+                 if(palabra==inverso)
+                {
+                    Console.WriteLine("Es palindrome");
+                }
+                 else
+                {
+                    Console.WriteLine(palabra + " no es palindrome");
+                }
+               
+                
+            Console.ReadKey();
+
+        }
+    }
+}
